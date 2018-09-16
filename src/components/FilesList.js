8b95fd76caeb6e34f6c381d5d819001995069d04
Peter Piskizhev <p.piskizhev@gmail.com>
2018-09-16 21:12:32 +0300

    update checkboxes

--- src/components/FilesList.js
+++ src/components/FilesList.js
@@ -9,34 +9,34 @@ import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-import styled from 'styled-components';
-
-const StyledListItem = styled(ListItem)`
-  position: relative;
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    width: 6px;
-    background-color: ${props => props.color};
-  }
-`;
+const styles = {
+  root: {
+    color: 'var(--colorR)',
+    '&$checked': {
+      color: 'var(--colorR)',
+    },
+  },
+  checked: {}
+};
 
 const FileList = ({ files, classes, handleToggle, handleDelete }) => (
   <List>
     {Object.keys(files).map(name => (
-      <StyledListItem 
+      <ListItem 
         key={name}
         button
         disableRipple
         color={files[name].color}
+        style={{ '--colorR': files[name].color }}
         onClick={() => handleToggle(name)}
       >
         <Checkbox 
           disableRipple
           color='default'
+          classes={{
+            root: classes.root,
+            checked: classes.checked
+          }}
           checked={files[name].isActive}
         />
         <ListItemText>{name}</ListItemText>
@@ -45,7 +45,7 @@ const FileList = ({ files, classes, handleToggle, handleDelete }) => (
             <DeleteIcon />
           </IconButton>
         </ListItemSecondaryAction>
-      </StyledListItem>
+      </ListItem>
     ))}
   </List>
 );
@@ -56,4 +56,4 @@ FileList.propTypes = {
   handleDelete: PropTypes.func.isRequired
 }
 
-export default FileList;
+export default withStyles(styles)(FileList);
