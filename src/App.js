6d24bef9786627d432384337c02c2f36a94e3955
Peter Piskizhev <p.piskizhev@gmail.com>
2018-09-17 18:46:58 +0300

    fix local storage button

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends Component {
   };
 
   getFromLocalStorage = key => {
-    let value = localStorage.getItem(key);
+    let value = localStorage.getItem(key) || {};
     console.log(value);
     try {
       value = JSON.parse(value);
